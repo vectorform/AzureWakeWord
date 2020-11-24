dda2591b1ab15f23a705132d6d872f4645461ba0
Jeff Meador <jmeador@vectorform.com>
2020-11-24 17:10:32 -0500

    Creating branch to test out keyword + custom commands

--- app/src/main/java/com/microsoft/cognitiveservices/speech/samples/quickstart/MainActivity.kt
+++ app/src/main/java/com/microsoft/cognitiveservices/speech/samples/quickstart/MainActivity.kt
@@ -1,8 +1,4 @@
-//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
-//
-// <code>
+
 package com.microsoft.cognitiveservices.speech.samples.quickstart
 
 import android.Manifest.permission
@@ -10,51 +6,109 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.View
 import android.widget.TextView
-import com.microsoft.cognitiveservices.speech.KeywordRecognitionModel
-import com.microsoft.cognitiveservices.speech.KeywordRecognizer
-import com.microsoft.cognitiveservices.speech.ResultReason
+import com.microsoft.cognitiveservices.speech.*
 import com.microsoft.cognitiveservices.speech.audio.AudioConfig
-import java.util.concurrent.Executors
+import com.microsoft.cognitiveservices.speech.dialog.ActivityReceivedEventArgs
+import com.microsoft.cognitiveservices.speech.dialog.CustomCommandsConfig
+import com.microsoft.cognitiveservices.speech.dialog.DialogServiceConnector
+import org.json.JSONObject
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
-    private val es = Executors.newFixedThreadPool(2)
+
+    private lateinit var dialogServiceConnector: DialogServiceConnector
+
+    init {
+        initSpeechSDK()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Note: we need to request the permissions
-        val requestCode = 5 // unique code for the permission request
-        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission.RECORD_AUDIO, permission.INTERNET), requestCode)
+        val statusTextView = findViewById<TextView>(R.id.status)
+        statusTextView.text = "listening for \"computer\""
+
+        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission.RECORD_AUDIO, permission.INTERNET), 123)
     }
 
-    fun onSpeechButtonClicked(v: View?) {
-        val txt = findViewById<View>(R.id.hello) as TextView // 'hello' is the ID of your text view
-        txt.text = "Say \"Computer\""
-        val asyncTask = Runnable {
-            try {
-                val am = assets
-                val config = AudioConfig.fromDefaultMicrophoneInput()!!
-                val reco = KeywordRecognizer(config)
-                val `is` = am.open("kws.table")
-                val model = KeywordRecognitionModel.fromStream(`is`, "computer", false)
-                val task = reco.recognizeOnceAsync(model)!!
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-                // Note: this will block the UI thread, so eventually, you want to
-                //        register for the event (see full samples)
-                val result = task.get()!!
-                if (result.reason == ResultReason.RecognizedKeyword) {
-                    txt.text = "Recognized " + result.text
-                } else {
-                    txt.text = "Error: got the wrong sort of recognition."
-                }
-                reco.close()
-            } catch (ex: Exception) {
-                Log.e("SpeechSDKDemo", "unexpected " + ex.message)
-                assert(false)
+        startKeywordListeningAsync()
+    }
+
+    private fun initSpeechSDK(){
+
+
+        val dialogServiceConfig = CustomCommandsConfig.fromSubscription(
+                BuildConfig.TEST_APP_ID,
+                BuildConfig.TEST_SPEECH_RESOURCE_KEY,
+                BuildConfig.TEST_APP_REGION
+        )
+
+        dialogServiceConfig.setProperty(PropertyId.SpeechServiceConnection_RecoLanguage, "en-us")
+
+        val audioConfig = AudioConfig.fromDefaultMicrophoneInput()
+
+        dialogServiceConnector = DialogServiceConnector(dialogServiceConfig, audioConfig)
+
+        dialogServiceConnector.recognizing.addEventListener { _: Any?, speechRecognitionResultEventArgs: SpeechRecognitionEventArgs ->
+
+            val statusTextView = findViewById<TextView>(R.id.status)
+            val reason = speechRecognitionResultEventArgs.result.reason
+            val text = speechRecognitionResultEventArgs.result.text
+            val message = "$reason $text"
+            if (reason == ResultReason.RecognizingKeyword) {
+                statusTextView.text = message
+            } else {
+                statusTextView.text = statusTextView.text.toString() + "\n" + message
             }
+
+            Log.i("KEYWORDTEST",message)
         }
-        es.execute(asyncTask)
+
+        dialogServiceConnector.recognized.addEventListener { _: Any?, speechRecognitionResultEventArgs: SpeechRecognitionEventArgs ->
+
+            val statusTextView = findViewById<TextView>(R.id.status)
+            val reason = speechRecognitionResultEventArgs.result.reason
+            val text = speechRecognitionResultEventArgs.result.text
+            val message = "$reason $text"
+            statusTextView.text = statusTextView.text.toString() + "\n" + message
+            Log.i("KEYWORDTEST",message)
+        }
+
+        dialogServiceConnector.sessionStarted.addEventListener { _: Any?, sessionEventArgs: SessionEventArgs ->
+            Log.i("KEYWORDTEST","SESSION STARTED $sessionEventArgs.sessionId")
+        }
+
+        dialogServiceConnector.sessionStopped.addEventListener { _: Any?, sessionEventArgs: SessionEventArgs ->
+            Log.i("KEYWORDTEST","SESSION STOPPED $sessionEventArgs.sessionId")
+        }
+
+        dialogServiceConnector.canceled.addEventListener { _: Any?, canceledEventArgs: SpeechRecognitionCanceledEventArgs ->
+
+            val errorCode = canceledEventArgs.errorCode.value
+            val errorDetails = canceledEventArgs.errorDetails
+
+            Log.i("KEYWORDTEST","CANCELLED ERROR $errorCode DETAILS $errorDetails")
+        }
+
+        dialogServiceConnector.activityReceived.addEventListener { _: Any?, activityEventArgs: ActivityReceivedEventArgs ->
+
+            val statusTextView = findViewById<TextView>(R.id.status)
+            val activity = activityEventArgs.activity
+            Log.i("KEYWORDTEST","RECEIVED ACTIVITY $activity")
+            val json = JSONObject(activityEventArgs.activity)
+            val event = json.getString("event")
+            statusTextView.text = statusTextView.text.toString() + "\n RECEIVED EVENT " + event + "\n listening for \"computer\""
+        }
+    }
+
+    private fun startKeywordListeningAsync(){
+        val inputStream: InputStream = assets.open("kws.table")
+        val model = KeywordRecognitionModel.fromStream(inputStream, "computer", false)
+        dialogServiceConnector.startKeywordRecognitionAsync(model)
     }
 }
